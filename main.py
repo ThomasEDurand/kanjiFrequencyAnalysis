39918bd981252271791cd6f0ede1d56e2bcc6527
tedurand <tedurand@live.unc.edu>
2022-02-04 23:21:30 -0500

    Initial Jisho lookup complete

--- main.py
+++ main.py
@@ -10,10 +10,8 @@ clpbrd = pyperclip.paste()
 parsed = kakasi.convert(clpbrd)
 
 # TODO
-# Convert verbs to stem form
 # Find better Frequency List
-# CONFIGURE CHROME PATHS
-# Condense definition
+# Condense definitions
 
 kanjiWords = []
 
@@ -23,49 +21,61 @@ f.close()
 
 for p in parsed:
     orig = p['orig']
-    if orig != p['kana'] and orig != p['hira']:  # check if word contains kanji
+    hira = p['hira']
+    if orig != p['kana'] and orig != p['hira'] and orig != '\r' and orig != '\n':  # check if word contains kanji
         wordFound = False
         for i in range(0, len(kanjiWords)):
             if kanjiWords[i][0] == orig:
-                kanjiWords[i] = (kanjiWords[i][0], kanjiWords[i][1] + 1)
+                kanjiWords[i] = (kanjiWords[i][0], kanjiWords[i][1] ,kanjiWords[i][2] + 1)
                 wordFound = True
 
         if not wordFound:
-            kanjiWords.append((orig, 1))
+            kanjiWords.append((orig, hira, 1))
 
 print(kanjiWords)
-
 
 for i in range(0, len(kanjiWords)):
     for j in range(0, len(wordFreq)):
         if kanjiWords[i][0] == wordFreq[j]:
-            kanjiWords[i] = (kanjiWords[i][0], kanjiWords[i][1] * (j + 1))
+            kanjiWords[i] = (kanjiWords[i][0], kanjiWords[i][1], kanjiWords[i][2] * (j + 1))
 
-kanjiWords.sort(key=lambda y: y[1], reverse=True)
+kanjiWords.sort(key=lambda y: y[2], reverse=True)
 print(kanjiWords)
 
-URL = "https://jisho.org/search/"
+numWords = 0
+if len(kanjiWords) > 10:
+    print("Found " + str(len(kanjiWords)) + " words written with kanji")
 
-# CHROME PATH CONFIGS WILL NEED TO CHANGE ON OTHER COMPUTERS
-chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-d = r'C:\Users\Thomas\PycharmProjects\jpnWFA\results'
-title = "result"
-t = title + '.html'
+    try:
+        numWords = int(input("How many words to look up (hit 0 for all): "))
+    except:
+        numWords = min(len(kanjiWords), 10)
+        print("Invalid input, searching " + str(numWords) + " words")
 
-path = os.path.join(d, t)
+    if numWords == 0:
+        numWords = len(kanjiWords)
 
-print("Found " + str(len(kanjiWords)) + " words written with kanji")
-numWords = int(input("How many words to look up (hit 0 for all): "))
-if numWords == 0:
+else:
     numWords = len(kanjiWords)
+
+try:
+    t = input("Save as: ") + '.html'
+except:
+    t = 'result.html'
+
+# CHROME PATH CONFIGS MAY NEED CHANGE ON OTHER SYSTEMS
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+d = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+path = os.path.join(d, t)
 
 with Session() as s:
     f = open(path, "w+", encoding='utf-8')
     for i in range(0, numWords):
         currentWord = kanjiWords[i][0]
-        URL = "https://jisho.org/word/" + currentWord
+        currentKana = kanjiWords[i][1]
+        URL = "https://jisho.org/search/" + currentWord
         print(URL)
-        f.write('<p><b>' + str(i+1) + " " + currentWord + '</b></p>' + "\n")
+        f.write('<p><b>' + str(i+1) + ". " + currentWord + ": " + currentKana + '</b></p>' + "\n")
         try:
             req = s.get(URL)
             soup = BeautifulSoup(req.content, 'html.parser')
@@ -75,7 +85,7 @@ with Session() as s:
                 f.write('<p>' + e.text + '</p>')
 
         except:
-            print("Error searching word")
+            print("Error searching word " + currentWord)
 
         f.write("\n")
 
