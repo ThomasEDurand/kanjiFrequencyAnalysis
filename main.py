c3fa137990e71c33e5c431b187ce8104b85797d5
tedurand <tedurand@live.unc.edu>
2022-02-24 16:28:34 -0500

    Cleaned up code, w functions

--- main.py
+++ main.py
@@ -13,71 +13,7 @@ from requests import Session
 from tkinter import filedialog
 
 
-def main():
-    kakasi = pykakasi.kakasi()
-    parsed = kakasi.convert(pyperclip.paste())
-
-    try:
-        if int(input("0 to use text from clipboard, 1 to chose local file: ")):
-            root = tk.Tk()
-            root.withdraw()
-            file_path = filedialog.askopenfilename()
-            print(file_path)
-            with open(file_path, "r", encoding='utf-8') as f:
-                parsed = kakasi.convert(f.read().replace('\n', ''))
-            f.close()
-    except:
-        print("Invalid input, using text from clipboard")
-
-    with open("Frequency.txt", "r", encoding='utf-8') as f:
-        wordFreq = f.read().splitlines()
-    f.close()
-
-    kanjiWords = []
-    specialChars = ['\r', '\n', '｢', '｣']  # special chars that kakasi picks up sometimes
-    for p in parsed:
-        orig, hira = p['orig'], p['hira']
-        if orig != p['kana'] and orig != p['hira'] and orig not in specialChars:  # check if word contains kanji
-            wordFound = False
-            for i in range(0, len(kanjiWords)):
-                if kanjiWords[i][0] == orig:
-                    kanjiWords[i][2] = kanjiWords[i][2] + 1
-                    wordFound = True
-
-            kanjiWords.append([orig, hira, 1]) if not wordFound else None
-
-    print(kanjiWords)
-
-    for i in range(0, len(kanjiWords)):
-        wordFound = False
-        for j in range(0, len(wordFreq)):
-            if kanjiWords[i][0] == wordFreq[j]:
-                kanjiWords[i] = (kanjiWords[i][0], kanjiWords[i][1], kanjiWords[i][2] * (j + 1))  # no sigmoid
-                wordFound = True
-        if not wordFound:
-            kanjiWords[i] = (kanjiWords[i][0], kanjiWords[i][1], kanjiWords[i][2] * 21000)  # no sigmoid
-
-    try:
-        if int(input("0 to sort by order in text, 1 to sort by obscurity: ")):
-            kanjiWords.sort(key=lambda y: y[2], reverse=True)
-            print(kanjiWords)
-    except:
-        kanjiWords.sort(key=lambda y: y[2], reverse=True)
-        print("Error, sorting order")
-
-    numWords = 0
-    if len(kanjiWords) > 10:
-        print("Found " + str(len(kanjiWords)) + " words written with kanji")
-        try:
-            numWords = int(input("How many words to look up (hit 0 for all): "))
-            if numWords == 0:
-                numWords = len(kanjiWords)
-        except:
-            numWords = min(len(kanjiWords), 10)
-            print("Invalid input, searching " + str(numWords) + " words")
-    else:
-        numWords = len(kanjiWords)
-
+def assembleKanjiDict(kanjiWords, numWords):
     try:
         t = input("Save as: ") + '.html'
     except:
@@ -106,6 +42,7 @@ def main():
                     if "Wikipedia definition" in e.text:
                         break
                     else:
+                        # Based on the format of jisho.org such that entry number and entry are printed on the same line
                         if k % 2 == 0:
                             f.write('' + e.text + ': ')
                             k += 1
@@ -121,6 +58,74 @@ def main():
         f.close()
     s.close()
     webbrowser.get(chrome_path).open(path)
+
+
+def obscuritySort(kanjiWords):
+    with open("Frequency.txt", "r", encoding='utf-8') as f:
+        wordFreq = f.read().splitlines()
+    f.close()
+
+    for i, word in enumerate(kanjiWords):
+        wordFound = False
+        for j in range(0, len(wordFreq)):
+            if kanjiWords[i][0] == wordFreq[j]:
+                kanjiWords[i][2] *= (j + 1)
+                wordFound = True
+        if not wordFound:
+            kanjiWords[i][2] *= 21000
+
+
+
+def main():
+    kakasi = pykakasi.kakasi()
+    parsed = kakasi.convert(pyperclip.paste())
+
+    try:
+        if int(input("0 to use text from clipboard, 1 to chose local file: ")):
+            root = tk.Tk()
+            root.withdraw()
+            file_path = filedialog.askopenfilename()
+            print(file_path)
+            with open(file_path, "r", encoding='utf-8') as f:
+                parsed = kakasi.convert(f.read().replace('\n', ''))
+            f.close()
+    except:
+        print("Invalid input, using text from clipboard")
+
+    kanjiWords = []
+    specialChars = ['\r', '\n', '｢', '｣']  # special chars that kakasi picks up sometimes
+    # Assemble kanjiWords from parsed
+    for p in parsed:
+        orig, hira = p['orig'], p['hira']
+        if orig != p['kana'] and orig != p['hira'] and orig not in specialChars:  # check if word contains kanji
+            wordFound = False
+            for i, word in enumerate(kanjiWords):
+                if word[0] == orig:
+                    kanjiWords[i][2] += 1
+                    wordFound = True
+            kanjiWords.append([orig, hira, 1]) if not wordFound else None
+
+    try:
+        if int(input("0 to sort by order in text, 1 to sort by obscurity: ")):
+            obscuritySort(kanjiWords)
+    except:
+        kanjiWords.sort(key=lambda y: y[2], reverse=True)
+        print("Error, sorting order")
+
+    print(kanjiWords)
+
+    numWords = len(kanjiWords)
+    if numWords > 10:
+        print("Found " + str(numWords) + " words written with kanji")
+        try:
+            numWords = int(input("How many words to look up (hit 0 for all): "))
+            if numWords == 0:
+                numWords = len(kanjiWords)
+        except:
+            numWords = min(len(kanjiWords), 10)
+            print("Invalid input, searching " + str(numWords) + " words")
+
+    assembleKanjiDict(kanjiWords, numWords)
 
 
 if __name__ == "__main__":
